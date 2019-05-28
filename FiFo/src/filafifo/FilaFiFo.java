0120b55769d680850de7750925baf1090cf80610
Luan G <luangrezende@gmail.com>
2019-05-28 09:59:13 -0300

    update fifo

--- FiFo/src/filafifo/FilaFiFo.java
+++ FiFo/src/filafifo/FilaFiFo.java
@@ -2,13 +2,17 @@ package filafifo;
 import Classes.pessoa;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 public class FilaFiFo {
     public static void main(String[] args) {
         Metodos mtd = new Metodos();
         
-        ArrayList<pessoa> user = new ArrayList<>();
-        
+        List<pessoa> user = new ArrayList<>();
+        for(int i = 1; i <= lista.length; i++) {
+
+        }
+
         System.out.println(user[0].getNome());
         
         //JOptionPane.showMessageDialog(null, mtd.adicionaFila(user), "Monta fila", 1);
